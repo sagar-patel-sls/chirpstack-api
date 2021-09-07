7099a117e0558bf674ec3ebd7cbe070778ed638d
Orne Brocaar <info@brocaar.com>
2021-09-07 15:16:28 +0530

    Refactor multicast groups APIs.
    Starting with ChirpStack Application Server v3.16+, multicast groups now
    belong to an application to enforce that devices from multiple applications
    can't be mixed into a single multicast group.
    
    Instead of providing a service_profile_id, one now has to provide an
    application_id when creating a multicast-group.
    
    Use this API version when upgrading to ChirpStack Application Server
    v3.16+. For older versions, use the previous API version(s).

--- python/src/setup.py
+++ python/src/setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 setup(
     name='chirpstack-api',
-    version = "3.9.6",
+    version = "3.9.7",
     url='https://github.com/sagar-patel-sls/chirpstack-api',
     author='Orne Brocaar',
     author_email='info@brocaar.com',
