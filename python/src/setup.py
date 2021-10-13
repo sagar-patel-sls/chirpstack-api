8d5325d0347b4b56181f37761638d0ce66c8a854
Orne Brocaar <info@brocaar.com>
2021-10-13 11:37:30 +0530

    Add AS923_4 region

--- python/src/setup.py
+++ python/src/setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 setup(
     name='chirpstack-api',
-    version = "3.10.0",
+    version = "3.10.1",
     url='https://github.com/sagar-patel-sls/chirpstack-api',
     author='Orne Brocaar',
     author_email='info@brocaar.com',
